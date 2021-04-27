038a9dfa18df284c97df96617b8078a0e318a9c2
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-27 13:43:54 -0700

    initial commit - starter code no rq

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Query Test 1
+
+The code in this repository is base on the
+[React Query Tutorial | Data Fetching with React-Query](https://youtu.be/46vKqPlTW2w)
+video.
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false);
+      setLoading(true);
+      try {
+        const response = await axios("https://random.dog/woof.json");
+        setData(response.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isError) return <h1>Error, try again!</h1>;
+  if (isLoading) return <h1>Loading ...</h1>;
+  return (
+    <div>
+      <img src={data.url} alt="dog" />
+    </div>
+  );
+}
+
+export default App;
