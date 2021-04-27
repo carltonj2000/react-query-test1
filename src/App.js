1cea6bb899d60151e35b2af492fdb8f10e2182dc
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-27 13:57:34 -0700

    add rq

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import axios from "axios";
 
 function App() {
-  const [isLoading, setLoading] = useState(false);
-  const [isError, setError] = useState(false);
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setError(false);
-      setLoading(true);
-      try {
-        const response = await axios("https://random.dog/woof.json");
-        setData(response.data);
-      } catch (error) {
-        setError(true);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-
-  if (isError) return <h1>Error, try again!</h1>;
+  const { data, error, isLoading } = useQuery("dogs", () =>
+    axios("https://random.dog/woof.json")
+  );
+  if (error) return <h1>Error, try again! {error}</h1>;
   if (isLoading) return <h1>Loading ...</h1>;
   return (
     <div>
-      <img src={data.url} alt="dog" />
+      <img src={data.data.url} alt="dog" />
     </div>
   );
 }
